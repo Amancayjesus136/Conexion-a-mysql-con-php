a5dab2b6d02d08ddecf641c60e7302a05310fe91
jottahs <jottahs136@gmail.com>
2023-06-28 02:21:50 +0200

    formulario

--- index.php
+++ index.php
@@ -1,19 +1,131 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registrar usuario</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+  <title>Sistema de Reserva de Citas</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <style>
+    body {
+      background-color: skyblue;
+    }
+
+    .container {
+      background-color: #ffffff;
+      margin-top: 50px;
+      padding: 30px;
+      border-radius: 5px;
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+      color: #007bff;
+      margin-bottom: 30px;
+      text-align: center;
+    }
+
+    label {
+      font-weight: bold;
+    }
+
+    .form-control {
+      border-color: #007bff;
+    }
+
+    .btn-primary {
+      background-color: #007bff;
+      border-color: #007bff;
+    }
+
+    .btn-primary:hover {
+      background-color: #0069d9;
+      border-color: #0062cc;
+    }
+
+    .logo {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 30px;
+    }
+
+    .logo img {
+      width: 200px;
+      height: auto;
+    }
+  </style>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $("input, select").on("input", function() {
+        var $field = $(this);
+        if ($field.val() !== "") {
+          $field.closest(".form-group").next().show();
+        } else {
+          $field.closest(".form-group").nextAll().hide();
+        }
+      });
+
+      $("#fecha_hora").on("input", function() {
+        var selectedDateTime = new Date($(this).val());
+        var unavailableDateTime = new Date("2023-06-29T15:00");
+
+        if (selectedDateTime.getTime() === unavailableDateTime.getTime()) {
+          alert("La fecha y hora seleccionada no está disponible.");
+          $(this).val("");
+        }
+      });
+    });
+  </script>
 </head>
 <body>
-    <form method="post">
-    	<h1>¡Suscribete!</h1>
-    	<input type="text" name="name" placeholder="Nombre completo">
-    	<input type="email" name="email" placeholder="Email">
-    	<input type="submit" name="register">
+  <div class="container">
+    <div class="logo">
+      <img src="img/ceeri-logo.jpg" alt="Logo de la empresa">
+    </div>
+  <div class="container">
+    <h1>Sistema de Reserva de Citas</h1>
+    <form action="registrar.php" method="POST">
+      <div class="form-group">
+        <label for="tipo_documento">Tipo de Documento:</label>
+        <select class="form-control" id="tipo_documento" name="tipo_documento" required>
+          <option value="">Seleccione</option>
+          <option value="DNI">DNI</option>
+          <option value="Pasaporte">Pasaporte</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="numero_documento">Número de Documento:</label>
+        <input type="number" class="form-control" id="numero_documento" name="numero_documento" required>
+      </div>
+      <div class="form-group">
+        <label for="nombres">Nombres:</label>
+        <input type="text" class="form-control" id="nombres" name="nombres" required>
+      </div>
+      <div class="form-group">
+        <label for="apellidos">Apellidos:</label>
+        <input type="text" class="form-control" id="apellidos" name="apellidos" required>
+      </div>
+      <div class="form-group">
+        <label for="telefono">Teléfono:</label>
+        <input type="number" class="form-control" id="telefono" name="telefono" required>
+      </div>
+      <div class="form-group">
+        <label for="especialidad">Especialidad:</label>
+        <input type="text" class="form-control" id="especialidad" name="especialidad" required>
+      </div>
+      <div class="form-group">
+        <label for="genero">Género:</label>
+        <select class="form-control" id="genero" name="genero" required>
+          <option value="">Seleccione</option>
+          <option value="Masculino">Masculino</option>
+          <option value="Femenino">Femenino</option>
+          <option value="Otro">Otro</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="fecha_hora">Fecha y Hora:</label>
+        <input type="datetime-local" class="form-control" id="fecha_hora" name="fecha_hora" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Reservar Cita</button>
     </form>
-        <?php 
-        include("registrar.php");
-        ?>
+  </div>
 </body>
 </html>
--- registrar.php
+++ registrar.php
@@ -1,28 +1,28 @@
-<?php 
-
+<?php
 include("con_db.php");
 
-if (isset($_POST['register'])) {
-    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
-	    $name = trim($_POST['name']);
-	    $email = trim($_POST['email']);
-	    $fechareg = date("d/m/y");
-	    $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
-	    $resultado = mysqli_query($conex,$consulta);
-	    if ($resultado) {
-	    	?> 
-	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
-           <?php
-	    } else {
-	    	?> 
-	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
-           <?php
-	    }
-    }   else {
-	    	?> 
-	    	<h3 class="bad">¡Por favor complete los campos!</h3>
-           <?php
-    }
-}
+if (isset($_POST['tipo_documento']) && isset($_POST['numero_documento']) && isset($_POST['nombres']) && isset($_POST['apellidos']) && isset($_POST['telefono']) && isset($_POST['especialidad']) && isset($_POST['genero']) && isset($_POST['fecha_hora'])) {
+    $tipo_documento = $_POST['tipo_documento'];
+    $numero_documento = $_POST['numero_documento'];
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $telefono = $_POST['telefono'];
+    $especialidad = $_POST['especialidad'];
+    $genero = $_POST['genero'];
+    $fecha_hora = $_POST['fecha_hora'];
+    $fechareg = date("Y-m-d H:i:s");
 
+    // Agregar aquí el código para verificar la disponibilidad de la fecha y hora seleccionadas
+
+    $consulta = "INSERT INTO citas (tipo_documento, numero_documento, nombres, apellidos, telefono, especialidad, genero, fecha_hora, fecha_registro) VALUES ('$tipo_documento', '$numero_documento', '$nombres', '$apellidos', '$telefono', '$especialidad', '$genero', '$fecha_hora', '$fechareg')";
+    $resultado = mysqli_query($conex, $consulta);
+
+    if ($resultado) {
+        echo "<h3 class='ok'>¡Se ha reservado la cita correctamente!</h3>";
+    } else {
+        echo "<h3 class='bad'>¡Ups, ha ocurrido un error al reservar la cita!</h3>";
+    }
+} else {
+    echo "<h3 class='bad'>¡Por favor, complete todos los campos del formulario!</h3>";
+}
 ?>
